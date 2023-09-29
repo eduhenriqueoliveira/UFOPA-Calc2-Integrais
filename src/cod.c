bb4ab694cdf7818df60c345797aa01bd797ca830
Eduardo Henrique <eduhenriqueoliveira9@gmail.com>
2023-09-28 23:13:01 -0300

    Código com função inicial

--- src/cod.c
+++ src/cod.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
+#define FUNCAO(x) (1.0/(x))
+
+double func(double x){
+    return 1.0/x;
+}
+
+double deltaX(int b, int a, int n){
+    return (double)(b-a)/n;
+}
+
+double integralPorPontoMedio(int a, int b, int qntQuadrados, double (*funcao)(double)){
+    double dX = deltaX(b,a,qntQuadrados);
+
+    double resultado=0;
+    
+    for(double i=(2*a+dX)/2; i<=b;i+=dX){
+        resultado+=funcao(i);
+    }
+
+    return resultado*dX;
+}
 void main(){
-    printf("Sim");
+    printf("%lf\n", integralPorPontoMedio(1,2,5,func));
 }
